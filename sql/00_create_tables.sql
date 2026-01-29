f611aded9b4f971df6c565e9bd557b9e9cb18ae1
Jiyun <jiyunk920@gmail.com>
2026-01-29 20:07:39 +0900

    ✨  feat: update raw_flights schema with flight_key and datetime fields (#1)

--- sql/00_create_tables.sql
+++ sql/00_create_tables.sql
@@ -1,17 +1,22 @@
 CREATE TABLE `PROJECT_ID.flight_delay.raw_flights` (
-  airline_icao STRING, -- 항공사 ICAO 코드
-  airline_kr STRING, -- 항공사명 (한글)
-  flight_iata STRING, -- 항공편 IATA 코드
-  dep_airport_iata STRING, -- 출발 공항 IATA 코드
-  dep_airport_kr STRING, -- 출발 공항명 (한글)
-  status STRING, -- 운항 상태
-  status_remark STRING, -- 운항 상태 상세
-  status_remark_code STRING, -- 운항 상태 상세 코드
-  scheduled_time TIME, -- 예정 출발 시각
-  expected_time TIME, -- 예상 출발 시각
-  actual_time TIME, -- 실제 출발 시각
-  nature STRING,     -- 여객/화물
-  cargo_yn BOOL,     -- nature 기반 파생
+  flight_key STRING, -- ymd  + flight_iata
+
+  airline_icao STRING,
+  airline_kr STRING,
+  flight_iata STRING,
+
+  dep_airport_iata STRING,
+  dep_airport_kr STRING,
+
+  status STRING,
+  status_remark STRING,
+  status_remark_code STRING,
+
+  scheduled_dt DATETIME,
+  expected_dt  DATETIME,
+  actual_dt    DATETIME,
+
+  nature STRING,
   ymd DATE,
   collected_at TIMESTAMP
 )
