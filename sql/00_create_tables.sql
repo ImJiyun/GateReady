d5651c47927eef898507176a2db4c65947831bfc
Jiyun <jiyunk920@gmail.com>
2026-01-29 20:56:36 +0900

    ✨  feat: add apIata and apKr to raw_flights table for further analysis (#1)

--- sql/00_create_tables.sql
+++ sql/00_create_tables.sql
@@ -1,9 +1,12 @@
 CREATE TABLE `PROJECT_ID.flight_delay.raw_flights` (
   flight_key STRING, -- ymd  + flight_iata
 
-  airline_icao STRING,
-  airline_kr STRING,
-  flight_iata STRING,
+  airline_icao STRING, -- 항공사 ICAO 코드
+  airline_kr STRING,  -- 항공사 한글명
+  flight_iata STRING, -- 항공편 IATA 코드
+
+  arr_airport_iata STRING, -- 목적지 IATA (apIata)
+  arr_airport_kr   STRING, -- 목적지 한글명 (apKr)
 
   status STRING,
   status_remark STRING,
