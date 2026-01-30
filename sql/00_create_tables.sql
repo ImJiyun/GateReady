8511db4245b78a5bd87ad23168e75a4bdb099d4a
Jiyun <jiyunk920@gmail.com>
2026-01-30 16:32:59 +0900

    ✨  feat: create airlines table schema to BigQuery (#5)

--- sql/00_create_tables.sql
+++ sql/00_create_tables.sql
@@ -21,3 +21,24 @@ CREATE TABLE `PROJECT_ID.flight_delay.raw_flights` (
   collected_at TIMESTAMP
 )
 PARTITION BY ymd;
+
+CREATE TABLE `PROJECT_ID.flight_delay.airlines` (
+  airline_icao STRING, -- 항공사 ICAO 코드
+  airline_iata STRING, -- 항공사 IATA 코드
+  airline_kr STRING,   -- 항공사 한글명
+  airline_en STRING,    -- 항공사 영문명
+
+  city_kr STRING,    -- 본사 도시 한글명
+  city_en STRING,    -- 본사 도시 영문명
+
+  country_kr STRING,  -- 본사 국가 한글명
+  country_en STRING,   -- 본사 국가 영문명
+
+  continent_kr STRING, -- 본사 대륙 한글명
+  continent_en STRING,  -- 본사 대륙 영문명
+
+  business_model STRING, -- 항공사 비즈니스 모델 
+  operating_status STRING, -- 운영 상태 
+
+  collected_at TIMESTAMP -- 데이터 수집 시각
+); 
